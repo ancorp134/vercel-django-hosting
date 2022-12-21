1b973da72e002f34bbb2580ef686db36b4d4ef48
Ankit Tyagi <ancorp007@gmail.com>
2022-12-21 12:25:27 +0530

    update urls.py

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('example.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
