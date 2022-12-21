52d08bce807326eeedce9ffef92edef9e6c82add
Ankit Tyagi <ancorp007@gmail.com>
2022-12-21 12:29:57 +0530

    change database

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -74,24 +74,24 @@ WSGI_APPLICATION = 'vercel_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': "django.db.backends.postgresql_psycopg2",
-#         'HOST': "db.mwevfbuodjtouuvdedeq.supabase.co",
-#         'NAME': "postgres",
-#         'USER': "postgres",
-#         'PASSWORD': "hancorp@123",
-#         'PORT': "5432",
-
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'HOST': "db.mwevfbuodjtouuvdedeq.supabase.co",
+        'NAME': "postgres",
+        'USER': "postgres",
+        'PASSWORD': "hancorp@123",
+        'PORT': "5432",
+
+    }
+}
 
 
 # Password validation
